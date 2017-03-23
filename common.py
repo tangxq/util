78faca6fe0ac25a23d7d02d8a2f0dd91c053cf3a
tangxiaoqiang <601674217@qq.com>
2017-03-23 11:54:25 +0800

    update project

--- common.py
+++ common.py
@@ -2,6 +2,7 @@ __author__ = 'txq'
 """python3下一些常用的方法或工具类整理"""
 
 import requests
+import time
 
 
 def contentsize(url):
@@ -11,3 +12,17 @@ def contentsize(url):
         r = requests.head(url=r.headers['Location'], timeout=3.0)
     size = round(r.headers['Content-Length'] / 1024, 2)
     return size
+
+
+def retry_request(url, times=3):
+    """设置请求重试次数,如果请求http code!=200"""
+    i = 0
+    while times >= 0:
+        r = requests.get(url)
+        if r.status_code == 200:
+            return r.text
+            # break
+        times -= 1
+        print('重试第%d次' % i)
+        i += 1
+
